7e19b46404d5ad81d992815fda22aa219da07c26
Wouter50 <wouterdeboer5@gmail.com>
2022-12-11 15:41:34 +0100

    added scanner voor qr codes

--- app/src/main/java/com/example/selsovid/Home.kt
+++ app/src/main/java/com/example/selsovid/Home.kt
@@ -5,13 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+
 class Home : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    lateinit var imageView: ImageView
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    fun generateCode(view: View){
+        val text = "ayaya"
+        val encoder = BarcodeEncoder()
+        val bitmap = encoder.encodeBitmap(text, BarcodeFormat.QR_CODE, 400, 400)
+        imageView = view?.findViewById<ImageView>(R.id.myImageView)!!
+
+        if (imageView != null) {
+            imageView.setImageBitmap(bitmap)
+            //val toast = Toast.makeText(context, "epico", Toast.LENGTH_SHORT)
+            //toast.show()
+        } else {
+            imageView.setImageBitmap(bitmap)
+            val toast = Toast.makeText(context, "broke", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+
+
+
 
 }
--- app/src/main/java/com/example/selsovid/MainActivity.kt
+++ app/src/main/java/com/example/selsovid/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.selsovid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.selsovid.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    private val REQUEST_ID_MULTIPLE_PERMISSIONS = 7
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
+
 
         binding.bottomNavigation.setOnItemSelectedListener{
             when(it.itemId){
@@ -26,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+        checkAndroidVersion()
     }
 
     private fun replaceFragment(fragment: Fragment){
@@ -34,4 +47,40 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout,fragment)
         fragmentTransaction.commit()
     }
+
+    private fun checkAndroidVersion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkAndRequestPermissions()
+        } else {
+            // code for lollipop and pre-lollipop devices
+        }
+    }
+
+    private fun checkAndRequestPermissions(): Boolean {
+        val camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val write = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val read = ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        val listPermissionsNeeded: MutableList<String> = ArrayList()
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA)
+        }
+        if (write != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (read != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toTypedArray(),
+                REQUEST_ID_MULTIPLE_PERMISSIONS
+            )
+            return false
+        }
+        return true
+    }
+
+
+
 }

fd07c689b6d22c9ae16f2a665c03d420a0e5139c
Wouter50 <wouterdeboer5@gmail.com>
2023-01-16 11:52:14 +0100

    continuation of adding vc request + new logo

--- app/src/main/java/com/example/selsovid/SendRequest.kt
+++ app/src/main/java/com/example/selsovid/SendRequest.kt
@@ -1,11 +1,10 @@
 package com.example.selsovid
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -24,15 +23,35 @@ class SendRequest: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_send_request)
+        setContentView(R.layout.activity_send_request)
 
         var requestName = findViewById<EditText>(R.id.request_name)
         var sendRequestButton = findViewById<Button>(R.id.sendRequestButton)
+        var backbutton = findViewById<Button>(R.id.backButton)
+
+        backbutton.setOnClickListener{
+            val intent = Intent (this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
 
         sendRequestButton.setOnClickListener {
             requestString = requestName.text.toString()
+            requestString = "{" +
+                    "\"issuerId\": 57683268, " +
+                    "\"fromUser\": \"ssi.is.cool@gmail.com\", " +
+                    "\"requestText\": \"$requestString\", " +
+                    "\"attachedVCs\": [\"\", \"\"]" +
+                    "}"
             Toast.makeText(this, requestString, Toast.LENGTH_LONG).show()
-            connectToAPI()
+            val thread = Thread(Runnable {
+                println("thread started.")
+                connectToAPI()
+                Thread.sleep(5000)
+                println("thread done yay.")
+
+
+            })
+            thread.start()
             }
 
         listView = findViewById<View>(R.id.listView) as ListView
@@ -50,7 +69,7 @@ class SendRequest: Activity() {
         listView.adapter = adapter
 
         listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val dataModel: CustomAdapter.DataModel = dataModel!![position] as CustomAdapter.DataModel
+            val dataModel: CustomAdapter.DataModel = dataModel!![position]
             dataModel.checked = !dataModel.checked
             adapter.notifyDataSetChanged()
         }
@@ -64,14 +83,14 @@ class SendRequest: Activity() {
     fun connectToAPI(){
         val postBody = requestString.trimMargin()
         val client = OkHttpClient.Builder()
-            .readTimeout(3, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
             .build()
         val request = Request.Builder()
-            .url("wss://ssi.s.mees.io/api")
+            .url("https://ssi.s.mees.io/api")
             .post(postBody.toRequestBody(MEDIA_TYPE_MARKDOWN))
             .build()
         client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            if (!response.isSuccessful) throw IOException("Unexpected code $response, request: $postBody")
 
             println(response.body!!.string())
         }

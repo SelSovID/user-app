26eaa51aa7d48a721dc1bf16af45a51b0e7f89d1
Mees van Dijk <mees@mees.io>
2023-01-17 18:22:00 +0100

    Implement export

--- app/src/main/java/com/example/selsovid/SSICertUtilities.kt
+++ app/src/main/java/com/example/selsovid/SSICertUtilities.kt
@@ -1,5 +1,8 @@
 package com.example.selsovid
 
+import com.example.selsovid.data.SsiCert.SSICert
+import com.google.protobuf.kotlin.toByteString
+import java.io.StringWriter
 import java.security.Key
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
@@ -9,22 +12,44 @@ import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 
 
-class SSICert(
+class SSICertUtilities(
   val publicKey: RSAPublicKey,
   val credentialText: String,
   val ownerSignature: ByteArray,
   val parentSignature: ByteArray? = null,
-  val parent: SSICert? = null,
+  val parent: SSICertUtilities? = null,
 ) {
 
-  val export: String
-          get() = "[{\"parent\":\"${parent?.export}\"},{\"publicKey\":\"${publicKey.encoded}\"},{\"credentialText\":\"$credentialText\"},{\"ownerSignature\":\"${Base64.getEncoder().encodeToString(ownerSignature)}\"},{\"parentSignature\":\"${if (parentSignature == null) "null" else Base64.getEncoder().encodeToString(parentSignature)}\"}]"
+  private fun export(): SSICert {
+    var builder = SSICert.newBuilder()
+    builder.credentialText = credentialText
+
+    var publicKeyEncoded = Base64.getEncoder().encodeToString(publicKey.encoded)
+    val publicKeyStrWriter = StringWriter()
+    publicKeyStrWriter.write("-----BEGIN PUBLIC KEY-----\n");
+    publicKeyStrWriter.write(publicKeyEncoded)
+    publicKeyStrWriter.write("\n")
+    publicKeyStrWriter.write("-----END PUBLIC KEY-----\n");
+    publicKeyStrWriter.close();
+
+
+    builder.publicKey = publicKeyStrWriter.toString()
+    builder.ownerSignature = ownerSignature.toByteString()
+    if (parentSignature != null) {
+      builder.parentSignature = parentSignature.toByteString()
+    }
+    if (parent != null) {
+      builder.parent = parent.export()
+    }
+    return builder.build()
+  }
+
 
   val isSelfSigned: Boolean
     get() = parent == null
 
   override fun equals(other: Any?): Boolean {
-    return if (other is SSICert) {
+    return if (other is SSICertUtilities) {
       (
               this.isSelfSigned == other.isSelfSigned
                       && this.publicKey == other.publicKey
@@ -46,40 +71,40 @@ class SSICert(
       publicKey: RSAPublicKey,
       ownerPrivateKey: RSAPrivateKey,
       credentialText: String,
-    ): SSICert {
+    ): SSICertUtilities {
       val ownerSignature = encrypt(getOwnerSignableText(publicKey, credentialText), ownerPrivateKey)
-      return SSICert(publicKey, credentialText, ownerSignature, null, null)
+      return SSICertUtilities(publicKey, credentialText, ownerSignature, null, null)
     }
 
     fun create(
       publicKey: RSAPublicKey,
       ownerPrivateKey: RSAPrivateKey,
       credentialText: String,
-      parent: SSICert,
+      parent: SSICertUtilities,
       parentPrivateKey: RSAPrivateKey
-    ): SSICert {
+    ): SSICertUtilities {
       val ownerSignature = encrypt(getOwnerSignableText(publicKey, credentialText), ownerPrivateKey)
       val parentSignature = encrypt(
         getParentSignableText(parent, publicKey, credentialText, ownerSignature),
         parentPrivateKey
       )
-      return SSICert(publicKey, credentialText, ownerSignature, parentSignature, parent)
+      return SSICertUtilities(publicKey, credentialText, ownerSignature, parentSignature, parent)
     }
 
 //    fun import(exported: String): SSICert {
 //
 //    }
 
-    private fun getOwnerSignableText(publicKey: RSAPublicKey, credentialText: String): String {
-      val keyEncoded = publicKey.encoded.toString()
-      return "[{\"ownerPublicKey\":\"${keyEncoded}\"},{\"credentialText\":\"$credentialText\"}]"
-    }
-
-    private fun getParentSignableText(parent: SSICert, ownerPublicKey: RSAPublicKey, credentialText: String, ownerSignature: ByteArray): String {
-      val keyEncoded = ownerPublicKey.encoded.toString()
-      val ownerSignatureEncoded = Base64.getEncoder().encodeToString(ownerSignature)
-      return "[{\"parent\":\"${parent.export}\"},{\"owner\":\"$keyEncoded\"},{\"credentialText\":\"$credentialText\"},{\"ownerSignature\":\"$ownerSignatureEncoded\"}]"
-    }
+//    private fun getOwnerSignableText(publicKey: RSAPublicKey, credentialText: String): String {
+//      val keyEncoded = publicKey.encoded.toString()
+//      return "[{\"ownerPublicKey\":\"${keyEncoded}\"},{\"credentialText\":\"$credentialText\"}]"
+//    }
+//
+//    private fun getParentSignableText(parent: SSICertUtilities, ownerPublicKey: RSAPublicKey, credentialText: String, ownerSignature: ByteArray): String {
+//      val keyEncoded = ownerPublicKey.encoded.toString()
+//      val ownerSignatureEncoded = Base64.getEncoder().encodeToString(ownerSignature)
+//      return "[{\"parent\":\"${parent.export}\"},{\"owner\":\"$keyEncoded\"},{\"credentialText\":\"$credentialText\"},{\"ownerSignature\":\"$ownerSignatureEncoded\"}]"
+//    }
 
     @Throws(Exception::class)
     private fun encrypt(plainText: String, key: Key): ByteArray {
